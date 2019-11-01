2fb2fe5d51426c4d953274496eed17a62314c955
Julius <julius.n@gmx.net>
2019-11-01 21:07:29 +0100

    Adjusting swap memory script and other things

--- .scripts/unison_startup.sh
+++ .scripts/unison_startup.sh
@@ -7,10 +7,10 @@ function sync {
 	echo "syncing $1..." >> /tmp/unison_startup.log
 	/opt/bin/unison $1
 	if [ $? -eq 0 ]; then
-		sed -i -e "s/$1=./$1=1/g" $STATE_FILE
+		/usr/bin/sed -i -e "s/$1=./$1=1/g" $STATE_FILE
 		echo "sucessfully synced $1" >> /tmp/unison_startup.log
 	else
-		sed -i -e "s/$1=./$1=0/g" $STATE_FILE
+		/usr/bin/sed -i -e "s/$1=./$1=0/g" $STATE_FILE
 		echo "failed syncing $1" >> /tmp/unison_startup.log
 	fi
 }
@@ -19,7 +19,7 @@ function sync {
 source $HOME/.keychain/$HOSTNAME-sh >> /tmp/unison_startup.log 2>&1
 ssh-add -l >> /tmp/unison_startup.log 2>&1
 while [[ $? -ne 0 ]]; do
-	sed -i "1s/.*/active_wait/" $STATE_FILE
+	/usr/bin/sed -i "1s/.*/active_wait/" $STATE_FILE
 	sleep 300
 	/usr/bin/keychain $HOME/.ssh/id_ed25519 >> /tmp/unison_startup.log 2>&1
 	source $HOME/.keychain/$HOSTNAME-sh >> /tmp/unison_startup.log 2>&1
@@ -34,6 +34,6 @@ fi
 
 echo "Setting Profile to $PROFILE" >> /tmp/unison_startup.log 2>&1
 
-sed -i "1s/.*/active_sync/" $STATE_FILE
+/usr/bin/sed -i "1s/.*/active_sync/" $STATE_FILE
 sync documents_$PROFILE
-sed -i "1s/.*/idle/" $STATE_FILE
+/usr/bin/sed -i "1s/.*/idle/" $STATE_FILE
